7f58a517f22b36b18917f8a30bf27dc1e1993f51
mahmed03 <mahmoud.ahmed@icr.ac.uk>
2025-06-30 16:05:02 +0100

    basic testing main

--- test-submit.sh
+++ test-submit.sh
@@ -1,43 +1,49 @@
 #!/bin/bash
 
-#SBATCH -o test/test.out
-#SBATCH -e test/test.err
-#SBATCH -J test
+#SBATCH -o tests/tests.out
+#SBATCH -e tests/tests.err
+#SBATCH -J tests
 #SBATCH -p master-worker
 #SBATCH -t 120:00:00
 
-# Setup test directory
-mkdir -p test/ test/input
-cd test/
+# Load conda environment
+module load Java/17
+source $NXF_CONDA
 
-# Download test data
+# Setup tests
+curl -fsSL https://get.nf-test.com | bash
+./nf-test init
+./nf-test generate pipeline main.nf
+
+# Download input data
+mkdir -p tests tests/input
 URL="https://figshare.com/ndownloader/files"
 
-wget -c $URL/50487621 -O input/pheno.variants.vcf.gz
-wget -c $URL/50487624 -O input/pheno.variants.vcf.gz.tbi
+wget -c $URL/50487621 -O $PWD/tests/input/pheno.variants.vcf.gz
+wget -c $URL/50487624 -O $PWD/tests/input/pheno.variants.vcf.gz.tbi
 
-echo "cohort,file,index" > input/cohort_info.csv
-echo "cohort1,input/pheno.variants.vcf.gz,input/pheno.variants.vcf.gz.tbi" >> input/cohort_info.csv
+echo "cohort,file,index" > $PWD/tests/input/cohort_info.csv
+echo "cohort1,$PWD/tests/input/pheno.variants.vcf.gz,$PWD/tests/input/pheno.variants.vcf.gz.tbi" >> $PWD/tests/input/cohort_info.csv
 
-# Clean previous runs
-# rm -rf results/ work/ .nextflow*
-rm ./test.* ./*.html
+# Run tests
+./nf-test test tests/main.nf.test
 
-# Run nextflow
-module load Nextflow
+# # Run nextflow (example)
+# # nextflow run genomictools/select-cohort-variants -r main \
+# cd tests/
 
-# nextflow run houlstonlab/annotate-vcf-vep -r v0.1 \
-nextflow run ../main.nf \
-    --output_dir ./results/ \
-    -profile cluster,test_vep \
-    -params-file ../test-params.json \
-    -with-report ./report.html \
-    -with-timeline ./timeline.html \
-    -resume
+# # nextflow run houlstonlab/annotate-vcf-vep -r v0.1 \
+# nextflow run ../main.nf \
+#     --output_dir ./results/ \
+#     -profile cluster,test_vep \
+#     -params-file ../test-params.json \
+#     -with-report ./report.html \
+#     -with-timeline ./timeline.html \
+#     -resume
 
-# usage: nextflow run [ local_dir/main.nf | git_url ]  
-# These are the required arguments:
-#     -r            {main,dev,gha} to run specific branch
-#     -profile      {local,cluster} to run using differens resources
-#     -params-file  params.json to pass parameters to the pipeline
-#     -resume       To resume the pipeline from the last checkpoint
+# # usage: nextflow run [ local_dir/main.nf | git_url ]  
+# # These are the required arguments:
+# #     -r            {main,dev,gha} to run specific branch
+# #     -profile      {local,cluster} to run using differens resources
+# #     -params-file  params.json to pass parameters to the pipeline
+# #     -resume       To resume the pipeline from the last checkpoint

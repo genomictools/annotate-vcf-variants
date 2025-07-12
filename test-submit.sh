d5ff21ac928ad65cea8f998ead2093e6fc5c6e32
mahmed03 <mahmoud.ahmed@icr.ac.uk>
2025-07-12 23:41:35 +0100

    download test data from github

--- test-submit.sh
+++ test-submit.sh
@@ -10,36 +10,34 @@
 module load Java/17
 source $NXF_CONDA
 
-# Setup tests
-curl -fsSL https://get.nf-test.com | bash
-./nf-test init
-./nf-test generate pipeline main.nf
+# # Setup tests
+# curl -fsSL https://get.nf-test.com | bash
+# ./nf-test init
+# ./nf-test generate pipeline main.nf
 
 # Download input data
 mkdir -p tests tests/input
-URL="https://figshare.com/ndownloader/files"
 
-wget -c $URL/50487621 -O $PWD/tests/input/pheno.variants.vcf.gz
-wget -c $URL/50487624 -O $PWD/tests/input/pheno.variants.vcf.gz.tbi
+URL="https://raw.githubusercontent.com/genomictools/test-datasets/refs/heads/annotate-vcf-variants"
+wget -c $URL/pheno.variants.vcf.gz -O tests/input/pheno.variants.vcf.gz
+wget -c $URL/pheno.variants.vcf.gz.tbi -O tests/input/pheno.variants.vcf.gz.tbi
+wget -c $URL/cohort_info.csv-O tests/input/cohort_info.csv
 
-echo "cohort,file,index" > $PWD/tests/input/cohort_info.csv
-echo "cohort1,$PWD/tests/input/pheno.variants.vcf.gz,$PWD/tests/input/pheno.variants.vcf.gz.tbi" >> $PWD/tests/input/cohort_info.csv
-
-# Run tests
-./nf-test test tests/main.nf.test
+# # Run tests
+# ./nf-test test tests/main.nf.test
 
 # # Run nextflow (example)
-# # nextflow run genomictools/select-cohort-variants -r main \
-# cd tests/
+# nextflow run genomictools/select-cohort-variants -r main \
+cd tests/
 
-# # nextflow run houlstonlab/annotate-vcf-vep -r v0.1 \
-# nextflow run ../main.nf \
-#     --output_dir ./results/ \
-#     -profile cluster,test_vep \
-#     -params-file ../test-params.json \
-#     -with-report ./report.html \
-#     -with-timeline ./timeline.html \
-#     -resume
+# nextflow run houlstonlab/annotate-vcf-vep -r v0.1 \
+nextflow run ../main.nf \
+    --output_dir ./results/ \
+    -profile cluster,test_spliceai \
+    -params-file ../test-params.json \
+    -with-report ./report.html \
+    -with-timeline ./timeline.html \
+    -resume
 
 # # usage: nextflow run [ local_dir/main.nf | git_url ]  
 # # These are the required arguments:

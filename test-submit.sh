1dab7e5a0a5431ec9e8e18b06cdf96603dce3aa5
mahmed03 <mahmoud.ahmed@icr.ac.uk>
2025-10-22 08:28:34 +0100

    test using test-dataset

--- test-submit.sh
+++ test-submit.sh
@@ -6,40 +6,31 @@
 #SBATCH -p master-worker
 #SBATCH -t 120:00:00
 
-# Load conda environment
-module load Java/17
-source $NXF_CONDA
+# Setup test directory
+mkdir -p tests/
 
-# # Setup tests
-# curl -fsSL https://get.nf-test.com | bash
-# ./nf-test init
-# ./nf-test generate pipeline main.nf
+TESTDATA="git@github.com:genomictools/test-datasets.git"
+BRANCH="annotate-vcf-variants"
+SRC="tests/input"
 
-# Download input data
-mkdir -p tests tests/input
+git -C $SRC pull || \
+git clone -b $BRANCH $TESTDATA $SRC
 
-URL="https://raw.githubusercontent.com/genomictools/test-datasets/refs/heads/annotate-vcf-variants"
-wget -c $URL/pheno.variants.vcf.gz -O tests/input/pheno.variants.vcf.gz
-wget -c $URL/pheno.variants.vcf.gz.tbi -O tests/input/pheno.variants.vcf.gz.tbi
-wget -c $URL/cohort_info.csv -O tests/input/cohort_info.csv
+# Run nextflow
+module load Nextflow
 
-# # Run tests
-# ./nf-test test tests/main.nf.test
-
-# # Run nextflow (example)
-# nextflow run genomictools/select-cohort-variants -r main \
 cd tests/
 
-# nextflow run houlstonlab/annotate-vcf-vep -r v0.1 \
+# nextflow run genomictools/annotate-vcf-variants -r v0.1 \
 nextflow run ../main.nf \
     --output_dir ./results/ \
     -profile cluster,test \
     -params-file ../test-params.json \
     -resume
 
-# # usage: nextflow run [ local_dir/main.nf | git_url ]  
-# # These are the required arguments:
-# #     -r            {main,dev,gha} to run specific branch
-# #     -profile      {local,cluster} to run using differens resources
-# #     -params-file  params.json to pass parameters to the pipeline
-# #     -resume       To resume the pipeline from the last checkpoint
+# usage: nextflow run [ local_dir/main.nf | git_url ]  
+# These are the required arguments:
+#     -r            {main,dev,gha} to run specific branch
+#     -profile      {local,cluster} to run using differens resources
+#     -params-file  params.json to pass parameters to the pipeline
+#     -resume       To resume the pipeline from the last checkpoint
